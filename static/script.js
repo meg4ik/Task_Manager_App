da94d0eb46a012fd10380c8dafba2f3c74ea83ed
meg4ik <meg4mag@gmail.com>
2020-12-25 16:58:19 +0200

    add cute papers at html design

--- static/script.js
+++ static/script.js
@@ -1,16 +1,10 @@
 'use strict';
         (function (window, document, $) {
         function sendRequest() {
-            var request = $.ajax({
+            $.ajax({
+                type: "POST", 
                 url: 'http://127.0.0.1:5000{{clink}}',
-                data : $("#contentform").serialize(),
-                dataType: 'json',
-                type: 'post'
-            });
-
-
-            request.fail(function() {
-              alert("fail!")
+                data: $("#contentform").serialize(),
             });
         }
 
